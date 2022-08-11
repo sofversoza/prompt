04833685df14c2620e4ab9ef7b852258d47ffd73
sofiaversoza <sofversoza@gmail.com>
2022-08-11 13:32:40 -0700

    more css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import './App.css'
+import Navbar from './components/nav/Navbar'
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>prompt. This is the Header h1</h1>
-			<h2>This is the title of the blogs h2</h2>
-			<h3>This is the subtitle of the blogs h3</h3>
-			<br />
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
-				quasi commodi quam harum autem placeat aliquam ea suscipit, beatae
-				similique architecto maxime odit. Eveniet quo exercitationem maxime
-				omnis explicabo enim? Lorem ipsum dolor sit amet consectetur adipisicing
-				elit. Consectetur voluptates voluptatibus ab totam delectus culpa,
-				voluptas architecto quod facilis. Nesciunt exercitationem quo placeat?
-				Quis, non exercitationem quidem iste recusandae sequi.
-			</p>
-			<br />
-			<button>Post</button>
-			<br />
-			<br />
-			<button>Send</button>
-		</div>
+		<>
+			<Navbar />
+			<div className='App'>
+				<h1>prompt. This is the Header h1</h1>
+				<h2>This is the title of the blogs h2</h2>
+				<h3>This is the subtitle of the blogs h3</h3>
+				<br />
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
+					quasi commodi quam harum autem placeat aliquam ea suscipit, beatae
+					similique architecto maxime odit. Eveniet quo exercitationem maxime
+					omnis explicabo enim? Lorem ipsum dolor sit amet consectetur
+					adipisicing elit. Consectetur voluptates voluptatibus ab totam
+					delectus culpa, voluptas architecto quod facilis. Nesciunt
+					exercitationem quo placeat? Quis, non exercitationem quidem iste
+					recusandae sequi.
+				</p>
+				<br />
+				<button>Post</button>
+				<br />
+				<br />
+				<button>Send</button>
+			</div>
+		</>
 	)
 }
 
---
+++ src/components/nav/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css'
+
+function Navbar() {
+	return (
+		<div className='navbar'>
+			<nav>
+				<h1>this is the navbar comp</h1>
+			</nav>
+		</div>
+	)
+}
+
+export default Navbar
