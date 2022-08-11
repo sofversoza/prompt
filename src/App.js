125d6f9f8a683e0bc3c677d6bdbaeda5f47ca374
sofiaversoza <sofversoza@gmail.com>
2022-08-10 22:34:50 -0700

    added global css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>prompt. This is the Header h1</h1>
+			<h2>This is the title of the blogs h2</h2>
+			<h3>This is the subtitle of the blogs h3</h3>
+			<br />
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
+				quasi commodi quam harum autem placeat aliquam ea suscipit, beatae
+				similique architecto maxime odit. Eveniet quo exercitationem maxime
+				omnis explicabo enim? Lorem ipsum dolor sit amet consectetur adipisicing
+				elit. Consectetur voluptates voluptatibus ab totam delectus culpa,
+				voluptas architecto quod facilis. Nesciunt exercitationem quo placeat?
+				Quis, non exercitationem quidem iste recusandae sequi.
+			</p>
+			<br />
+			<button>Post</button>
+			<br />
+			<br />
+			<button>Send</button>
+		</div>
+	)
 }
 
-export default App;
+export default App
