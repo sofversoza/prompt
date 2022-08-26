7128e49ea5c89d4def57b22bdda2a5124649cdf2
sofiaversoza <sofversoza@gmail.com>
2022-08-25 22:18:41 -0700

    end of day push

--- src/components/nav/SidebarLinks.jsx
+++ src/components/nav/SidebarLinks.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'react'
+import { GiSpellBook } from 'react-icons/gi'
+import { BsPaperclip } from 'react-icons/bs'
+import { TiHome } from 'react-icons/ti'
+import { ImQuill } from 'react-icons/im'
+import { FaMapPin } from 'react-icons/fa'
+import { BsFillDropletFill } from 'react-icons/bs'
+import { GiFairyWand } from 'react-icons/gi'
 import './SidebarLinks.css'
-import create1 from '../../assets/create1.svg'
 
 export default function SidebarLinks() {
-  
+  const [showThemes, setShowThemes] = useState(false)
+
+  // const showThemesModal = () => {
+  //   setShowThemes(true)
+  // }
+
   return (
     <div className="side-links">
-      <img src={create1} className="icons"/>
+      <div className="icon-cont">
+        <TiHome 
+          className="icons"
+          onClick={() => setShowThemes(true)}
+        />
+        <ImQuill className="icons smaller"/>
+        {/* <BsPaperclip className="icons"/> */}
+        <FaMapPin className="icons" />
+        <GiSpellBook className="icons"/>
+      </div>
+      <hr/>
+      <div className="icon-themes">
+        <GiFairyWand className="icons smaller" />
+        <BsFillDropletFill className="icons smaller violet" />
+        <BsFillDropletFill className="icons smaller matcha" />
+        <BsFillDropletFill className="icons smaller fluff" />
+      </div>
     </div>
   )
 }
